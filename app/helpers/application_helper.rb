d90f24930e6f1304ffa62dd277378e454f833571
Eric Guo <eric.guo@sandisk.com>
2016-02-22 20:19:40 +0800

    No logic change, just style improve.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,36 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Fighting Technology Group Hub App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 
-	def full_title(page_title = '')
-		base_title = 'Fighting Technology Group Hub App'
-		if page_title.empty?
-			base_title
-		else
-			page_title + " | " + base_title
-		end
-	end
-
-	def flash_class_name(name)
+  def flash_class_name(name)
     case name
     when 'notice' then 'success'
     when 'alert'  then 'danger'
     else name
     end
-	end
+  end
 
-	def current_user?(user)
+  def current_user?(user)
     user == current_user
   end
 
   def current_profile?
-  	Profile.find_by(user_id: current_user).nil?
+    Profile.find_by(user_id: current_user).nil?
   end
 
   def current_profile
-  	Profile.find_by(user_id: current_user).id
+    Profile.find_by(user_id: current_user).id
   end
 
   def comment_count(fightpost)
     if Comment.find_by(fightpost_id: fightpost).nil?
-      "0"
+      '0'
     else
       Comment.where(fightpost_id: fightpost).count
     end
@@ -44,6 +43,4 @@ module ApplicationHelper
       @fightpost
     end
   end
-
-
 end
