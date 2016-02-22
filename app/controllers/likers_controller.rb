bf5276e0c24f8aff7660fabf81c43e05ae1a2a89
Eric Guo <eric.guo@sandisk.com>
2016-02-22 20:17:15 +0800

    Do not using fightpost to represent the fightpost_id

--- app/controllers/likers_controller.rb
+++ app/controllers/likers_controller.rb
@@ -1,25 +1,24 @@
 class LikersController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def create
-		@fightpost = Fightpost.find(params[:fightpost_id])
-		current_user.likers.create(fightpost_id: @fightpost.id)
+  def create
+    @fightpost = Fightpost.find(params[:fightpost_id])
+    current_user.likers.create(fightpost_id: @fightpost.id)
 
-		respond_to do |format|
-			format.html { redirect_to @fightpost }
-			format.js
-		end
-	end
+    respond_to do |format|
+      format.html { redirect_to @fightpost }
+      format.js
+    end
+  end
 
-	def destroy
-		@fightpost = Liker.find(params[:id]).fightpost_id
-		current_user.likers.find_by(fightpost_id: @fightpost).destroy
-		@fightpost = Fightpost.find(@fightpost)
+  def destroy
+    fightpost_id = Liker.find(params[:id]).fightpost_id
+    current_user.likers.find_by(fightpost_id: fightpost_id).destroy
+    @fightpost = Fightpost.find(fightpost_id)
 
-		respond_to do |format|
-			format.html { redirect_to @fightpost }	
-			format.js
-		end
-	end
-
+    respond_to do |format|
+      format.html { redirect_to @fightpost }
+      format.js
+    end
+  end
 end
