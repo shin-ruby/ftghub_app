8a05f396256c8a313afbf17a007d26f5f24d73e4
Eric Guo <eric.guo@sandisk.com>
2016-02-24 22:21:04 +0800

    Leave mark TODO to fix raw SQL.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name , :on => :create, :message => "can't be blank"
+  validates_presence_of :name, on: :create, message: "can't be blank"
   has_many :fightposts, dependent: :destroy
-  has_many :active_relationships,  class_name:  "Relationship",
-                                   foreign_key: "follower_id",
+  has_many :active_relationships,  class_name:  'Relationship',
+                                   foreign_key: 'follower_id',
                                    dependent:   :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
+  has_many :passive_relationships, class_name:  'Relationship',
+                                   foreign_key: 'followed_id',
                                    dependent:   :destroy
 
   has_many :following, through: :active_relationships,  source: :followed
@@ -20,26 +20,21 @@ class User < ActiveRecord::Base
   has_one :profile, dependent: :destroy
   has_many :likers, dependent: :destroy
 
-
-
+  # TODO: use where.or after upgrade to Rails 5
   def feed
-    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
     Fightpost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
-  # 关注另一个用户
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
 
-  # 取消关注另一个用户
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
-  # 如果当前用户关注了指定的用户，返回 true
   def following?(other_user)
     following.include?(other_user)
   end
-  
 end
