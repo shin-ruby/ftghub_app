b73e3c26cf437646164b8446b3031e77e68d16ac
Eric Guo <eric.guo@sandisk.com>
2016-02-25 20:51:45 +0800

    Actually upgrade to Rails 5

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -9,7 +9,6 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 
   test 'profile display' do
     get user_path(@user)
-    assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
