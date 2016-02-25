7c8fad29026b80722d09590da7807db1a8089bc6
Eric Guo <eric.guo@sandisk.com>
2016-02-25 21:27:26 +0800

    Deprecation fix
    https://coderwall.com/p/rz2_kw/from-4-2-to-5-0-rails-migration-part-1

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-
-	def setup
+  def setup
     @user       = users(:michael)
     @other_user = users(:archer)
   end
 
-  test "should get show" do
-    get :show, id: @user
+  test 'should get show' do
+    get :show, params: { id: @user }
     assert_response :success
   end
-
 end
