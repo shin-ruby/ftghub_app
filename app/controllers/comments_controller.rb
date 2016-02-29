644a12b0da51544bc8df329684cd14d2fedf4cca
Eric Guo <eric.guo@sandisk.com>
2016-02-29 21:03:10 +0800

    Improve the comments in more rails way.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:content, :user_id)
+    params.require(:comment).permit(:content).merge(user: current_user)
   end
 end
